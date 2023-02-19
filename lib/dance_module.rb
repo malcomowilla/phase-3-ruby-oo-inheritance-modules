50eaed5d0a4c83cd58ec9b06bc03824997793047
malcomowilla <owillamalcom@gmail.com>
2023-02-19 16:54:55 +0300

    modules

---
+++ lib/dance_module.rb
@@ -0,0 +1,20 @@
+module Dance
+    def twirl
+        "I'm twirling!"
+    end
+
+    def jump
+        "Look how high i'm jumping!"
+    end
+    def pirouette
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. it was a pleasure to dance for you all."
+
+        
+    end
+end
+
+
